35ec690c4716ff9595d7b0fdcda1d6ec7c99ec44
thebe <thebest20067@gmail.com>
2025-10-01 22:15:05 -0700

    New Autos

--- src/main/java/net/forixaim/omneria/animations/battle_style/genesis_wyrm/GenesisWyrmAnimations.java
+++ src/main/java/net/forixaim/omneria/animations/battle_style/genesis_wyrm/GenesisWyrmAnimations.java
@@ -3,6 +3,7 @@ package net.forixaim.omneria.animations.battle_style.genesis_wyrm;
 import net.forixaim.omneria.animations.ReusableEvents;
 import net.forixaim.omneria.animations.types.BattleArtsAttackPhaseProperties;
 import net.forixaim.omneria.animations.types.OmneriaAttackAnimation;
+import net.forixaim.omneria.colliders.GenesisWyrmColliders;
 import yesman.epicfight.api.animation.AnimationManager;
 import yesman.epicfight.api.animation.property.AnimationEvent;
 import yesman.epicfight.api.animation.property.AnimationProperty;
@@ -31,7 +32,7 @@ public class GenesisWyrmAnimations
     public static void build(AnimationManager.AnimationBuilder builder)
     {
         AUTO1 = builder.nextAccessor("battle_style/legendary/genesis_wyrm/auto1", access -> new BasicAttackAnimation(
-                0.1f, 0.0f, 0.15f, 0.3f, 0.4f, ColliderPreset.FIST, Armatures.BIPED.get().handR, access, Armatures.BIPED
+                0.1f, 0.0f, 0.1f, 0.2f, 0.4f, GenesisWyrmColliders.GW_CLAW, Armatures.BIPED.get().handL, access, Armatures.BIPED
         ).addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.HOLD)
                 .addProperty(AnimationProperty.AttackPhaseProperty.SWING_SOUND, EpicFightSounds.SWORDMASTER_SWING.get())
                 .addProperty(AnimationProperty.AttackPhaseProperty.HIT_SOUND, EpicFightSounds.BLADE_HIT.get())
@@ -39,7 +40,7 @@ public class GenesisWyrmAnimations
                 .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED_MODIFIER, Animations.ReusableSources.CONSTANT_ONE));
 
         AUTO2 = builder.nextAccessor("battle_style/legendary/genesis_wyrm/auto2", access -> new BasicAttackAnimation(
-                0.1f, 0.0f, 0.2f, 0.3f, 0.4f, ColliderPreset.FIST, Armatures.BIPED.get().handL, access, Armatures.BIPED
+                0.1f, 0.0f, 0.1f, 0.2f, 0.4f, GenesisWyrmColliders.GW_CLAW, Armatures.BIPED.get().handR, access, Armatures.BIPED
         ).addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.HOLD)
                 .addProperty(AnimationProperty.AttackPhaseProperty.SWING_SOUND, EpicFightSounds.SWORDMASTER_SWING.get())
                 .addProperty(AnimationProperty.AttackPhaseProperty.HIT_SOUND, EpicFightSounds.BLADE_HIT.get())
@@ -47,12 +48,19 @@ public class GenesisWyrmAnimations
                 .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED_MODIFIER, Animations.ReusableSources.CONSTANT_ONE));
 
         AUTO3 = builder.nextAccessor("battle_style/legendary/genesis_wyrm/auto3", access -> new OmneriaAttackAnimation(
-                0.1f, 0.0f, 0.2f, 0.3f, 0.4f, ColliderPreset.FIST, Armatures.BIPED.get().handL, access, Armatures.BIPED
+                0.1f,access, Armatures.BIPED,
+                new AttackAnimation.Phase(0.0f, 0.0f, 0.1f, 0.15f, 0.15f, 0.15f, Armatures.BIPED.get().rootJoint, GenesisWyrmColliders.GW_CIRCLE_CLAW),
+                new AttackAnimation.Phase(0.15f, 0.0f, 0.15f, 0.2f, 0.2f, 0.4f, Armatures.BIPED.get().rootJoint, GenesisWyrmColliders.GW_CIRCLE_CLAW)
+                        .addProperty(AnimationProperty.AttackPhaseProperty.SWING_SOUND, EpicFightSounds.SWORDMASTER_SWING.get())
+                        .addProperty(AnimationProperty.AttackPhaseProperty.HIT_SOUND, EpicFightSounds.BLADE_HIT.get())
+
         ).addProperty(BattleArtsAttackPhaseProperties.KNOCKBACK_ANGLE, 25d)
                 .addProperty(BattleArtsAttackPhaseProperties.KNOCKBACK_POWER, 3.0d)
-                .addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.NEUTRALIZE)
-                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED_MODIFIER, Animations.ReusableSources.CONSTANT_ONE)
-                .addEvents(AnimationEvent.InTimeEvent.create(0.3f, ReusableEvents.FIRE_DRAGON_SHOT, AnimationEvent.Side.BOTH)));
+                .addProperty(AnimationProperty.AttackPhaseProperty.SWING_SOUND, EpicFightSounds.SWORDMASTER_SWING.get())
+                .addProperty(AnimationProperty.AttackPhaseProperty.HIT_SOUND, EpicFightSounds.BLADE_HIT.get())
+                .addProperty(AnimationProperty.AttackPhaseProperty.PARTICLE, EpicFightParticles.HIT_BLADE)
+                .addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.HOLD)
+                .addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED_MODIFIER, Animations.ReusableSources.CONSTANT_ONE));
 
         LEG_AUTO1 = builder.nextAccessor("battle_style/legendary/genesis_wyrm/leg_auto1", access -> new OmneriaAttackAnimation(
                 0.0f, access, Armatures.BIPED,
@@ -66,7 +74,7 @@ public class GenesisWyrmAnimations
         ).addProperty(AnimationProperty.StaticAnimationProperty.PLAY_SPEED_MODIFIER, Animations.ReusableSources.CONSTANT_ONE));
 
         LEG_AUTO2 = builder.nextAccessor("battle_style/legendary/genesis_wyrm/leg_auto2", access -> new OmneriaAttackAnimation(
-                0.1f, 0.0f, 0.1f, 0.2f, 0.4f, ColliderPreset.FIST, Armatures.BIPED.get().handL, access, Armatures.BIPED
+                0.1f, 0.0f, 0.1f, 0.2f, 0.4f, ColliderPreset.FIST, Armatures.BIPED.get().legL, access, Armatures.BIPED
         ).addProperty(BattleArtsAttackPhaseProperties.KNOCKBACK_ANGLE, 25d)
                 .addProperty(BattleArtsAttackPhaseProperties.KNOCKBACK_POWER, 3.0d)
                 .addProperty(AnimationProperty.AttackPhaseProperty.STUN_TYPE, StunType.NEUTRALIZE)
---
+++ src/main/java/net/forixaim/omneria/colliders/GenesisWyrmColliders.java
@@ -0,0 +1,23 @@
+package net.forixaim.omneria.colliders;
+
+import net.forixaim.omneria.VisitorsOfOmneria;
+import net.minecraft.resources.ResourceLocation;
+import yesman.epicfight.api.collider.Collider;
+import yesman.epicfight.api.collider.MultiOBBCollider;
+
+import static yesman.epicfight.gameasset.ColliderPreset.registerCollider;
+
+public class GenesisWyrmColliders
+{
+    private static ResourceLocation reg(String name)
+    {
+        return ResourceLocation.fromNamespaceAndPath(VisitorsOfOmneria.MOD_ID, name);
+    }
+    public static final Collider GW_CLAW = registerCollider(reg("gw_claw"), new MultiOBBCollider(5,
+            0.3D, 0.6D, 0.3D, 0.0D, 0.3D, -0.0D
+    ));
+
+    public static final Collider GW_CIRCLE_CLAW = registerCollider(reg("gw_circle_claw"), new MultiOBBCollider(5,
+            1.5D, 0.5D, 1.5D, 0.0D, 1D, -0.0D
+    ));
+}
